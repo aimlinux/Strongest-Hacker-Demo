c524b534addb7ddb2df75be9b452960b3e80ee21
aimlinux <114636143+aimlinux@users.noreply.github.com>
2023-04-09 16:18:00 +0900

    Source code on the left, etc...

--- Commander/script/main.js
+++ Commander/script/main.js
@@ -1,11 +1,18 @@
-var user_name = "mia";
+//ユーザーの名前を決める（inputにしたい...）
+var user_name = prompt("UserNameを入力してください。");
+if (user_name == 0) {
+    user_name = "NameLess"
+}
 
 var terminal = null;				
 var screenWidth, screenHeight;	
-var Initial_command = "mia@Commander:/home/" + user_name + "$ ";
+var Initial_command = user_name + "@Commander:/home/" + user_name + "$ ";
 var value = "";	// ターミナル画面上の文字列取得
 
-var first_command = "\n [sudo] password for " + user_name + " : ";
+var first_command = " [sudo] password for " + user_name + " : ";
+var space_command = " ";
+
+
 
 function setTerminal(){
 	// ブラウザのウインドウサイズを取得
@@ -29,11 +36,16 @@ window.addEventListener("load", function(){
 	terminal.focus();
 });
 
+
+
+
 window.addEventListener("resize", function(){
 	setTerminal();
 });
 
 
+
+//何かのキーが押されたとき
 window.addEventListener("keyup", function(e){
 	// キー番号を取得
 	let key = e.keyCode;
@@ -46,17 +58,132 @@ window.addEventListener("keyup", function(e){
 			return;										// 取得した文字の最後が改行文字ではないので何もしない
 		}
 
-        let num = Math.floor(Math.random() * 10) + 1;
-        alert(num);
+        
+        //ランダムに10桁のパスワード生成
+        let user_password = [];
+        for (let i = 0; i < 10; i++) {
+            user_password.push(Math.floor(Math.random() * 10));
+        }
 
-        num = 1;
 
+        //1~10までの乱数を生成する
+        let num = Math.floor(Math.random() * 10);
+        num = 0;
+        
+        //生成された乱数によって実行される処理を分ける
         switch (num) {
-            case 1:
-		        terminal.value = value + first_command;
-		        console.log("[" + value + "]");
-		        terminal.focus();
+            case 0:
+
+                (async () => {
+                    const sleep = (second) => new Promise(resolve => setTimeout(resolve, second * 1000))
+            
+                    await sleep(1)
+	                terminal.value = value + first_command;
+                    console.log("[" + value + "]");
+                    await sleep(0.7)
+
+                    terminal.value = value + first_command
+                    + user_password[0];
+                    console.log("[" + value + "]");
+                    await sleep(0.3)
+
+                    terminal.value = value + first_command
+                    + user_password[0]
+                    + user_password[1];
+                    console.log("[" + value + "]");
+                    await sleep(0.3)
+                    
+                    terminal.value = value + first_command
+                    + user_password[0]
+                    + user_password[1]
+                    + user_password[2];
+                    console.log("[" + value + "]");
+                    await sleep(0.2)
+                    
+                    terminal.value = value + first_command
+                    + user_password[0]
+                    + user_password[1]
+                    + user_password[2] 
+                    + user_password[3];
+                    console.log("[" + value + "]");
+                    await sleep(0.2)
+                    
+                    terminal.value = value + first_command
+                    + user_password[0]
+                    + user_password[1]
+                    + user_password[2] 
+                    + user_password[3]
+                    + user_password[4];
+                    console.log("[" + value + "]");
+                    await sleep(0.2)
+                    
+                    terminal.value = value + first_command
+                    + user_password[0]
+                    + user_password[1]
+                    + user_password[2] 
+                    + user_password[3]
+                    + user_password[4]
+                    + user_password[5];
+                    console.log("[" + value + "]");
+                    await sleep(0.2)
+                    
+                    terminal.value = value + first_command
+                    + user_password[0]
+                    + user_password[1]
+                    + user_password[2] 
+                    + user_password[3]
+                    + user_password[4]
+                    + user_password[5]
+                    + user_password[6];
+                    console.log("[" + value + "]");
+                    await sleep(0.2)
+                    
+                    terminal.value = value + first_command
+                    + user_password[0]
+                    + user_password[1]
+                    + user_password[2] 
+                    + user_password[3]
+                    + user_password[4]
+                    + user_password[5]
+                    + user_password[6]
+                    + user_password[7];
+                    console.log("[" + value + "]");
+                    await sleep(0.2)
+                    
+                    terminal.value = value + first_command
+                    + user_password[0]
+                    + user_password[1]
+                    + user_password[2] 
+                    + user_password[3]
+                    + user_password[4]
+                    + user_password[5]
+                    + user_password[6]
+                    + user_password[7]
+                    + user_password[8];
+                    console.log("[" + value + "]");
+                    await sleep(0.3)
+                    
+                    terminal.value = value + first_command
+                    + user_password[0]
+                    + user_password[1]
+                    + user_password[2] 
+                    + user_password[3]
+                    + user_password[4]
+                    + user_password[5]
+                    + user_password[6]
+                    + user_password[7]
+                    + user_password[8]
+                    + user_password[9];
+                    console.log("[" + value + "]");
+                    await sleep(1)
+
+                    terminal.value = terminal.value + "\n" + Initial_command;
+                    console.log("[" + value + "]");
+                    terminal.focus();
+                })()
+            
                 break;
+
 
             default:
                 terminal.value = value + Initial_command;
@@ -67,4 +194,8 @@ window.addEventListener("keyup", function(e){
 });
 
 
+
 window.addEventListener("")
+
+
+    
